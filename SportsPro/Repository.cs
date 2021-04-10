63c96e98bfbc20a175c90868773b7e97029e83ae
lynxAO <79384984+lynxAO@users.noreply.github.com>
2021-04-09 23:57:23 -0400

    Update Repository.cs

--- SportsPro/Repository.cs
+++ SportsPro/Repository.cs
@@ -1,11 +1,42 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace WebApplication4.Repository
+namespace SportsPro.Repository
 {
-    public class Class
+    public class Repository<T> : IRepository<T> where T : class
     {
+        protected SportsPro context { get; set; }
+        private DBSet<T> dbset { get; set; }
+        public Repository(SportsPro ctx)
+        {
+            context = ctx;
+            dbset = context.Set<T>();
+        }
+
+        public virtual IEnumberable<T> List(QueryOptions<T> options)
+        {
+            IQueryable<T> query = dbset; 
+            foreach (string include in options.GetIncludes())
+            {
+                query = query.Include(include);
+            }
+
+            if (options.HasWhere)
+                query = query.Where(options.where);
+            if (options.HasOrderBy)
+                query = query.OrderBy(options.OrderBy);
+            if options.HasPaging)
+                query = query.PageBy(options.PageNumber, options.PageSize);
+            return query.ToList();
+        }
+
+        public virtual T Get(int id) => dbset.Add(entity);
+        public virtual void Insert (T entity) = > dbset.Add(entity);
+        public virtual void Update(T entity) = > dbset.Update(entity);
+        public virtual void Delete(T entity) = > dbset.Remove(entity);
+        public virtual void Save() => context.SaveChanges();
     }
-}
+    
+ }
